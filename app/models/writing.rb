40bcf72fed7a44440c749c961ed1ad078a4ee498
Niko <artwarchol@gmail.com>
2019-04-17 23:37:09 +0300

    first step complite

--- app/models/writing.rb
+++ app/models/writing.rb
@@ -2,7 +2,24 @@ class Writing < ApplicationRecord
 	def search(title)
 	    ShikiAPI.search(title)
 	end
+
 	def searchDB(title)
-		Writing.where(name_en: title)
+		@data = Writing.where(Writing.arel_table[:name_en].matches(title))
+		if(!@data.empty?)
+			@data
+		else
+			search(title).each do |result|
+				@anime = Writing.where(Writing.arel_table[:shikimoryId].matches(result['id']))
+				if(@anime.empty?)
+					@moreInfo = ShikiAPI.anime(result['id'])
+					u = Writing.create :shikimoryId => result['id'], :my_anime_list_Id => @moreInfo['myanimelist_id'], :name_en => result['name'], :name_ru => @moreInfo['russian'], :description_ru => @moreInfo['description'], :series_count => result['episodes'], :name_jp => @moreInfo['japanese'][0], :genre =>  @moreInfo['genres'][0]['id'], :release_date => result['released_on'], :shikimory_img => result['image']['original']
+				else
+			end
+
+		end
+			#search(title)
+			#@writing = Writing.new()
+		end
 	end
+
 end
